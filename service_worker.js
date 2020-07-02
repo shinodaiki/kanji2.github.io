adc14ff9d4f1f4fc1999de52904028a326dd83f6
shinodaiki <67748501+shinodaiki@users.noreply.github.com>
2020-07-03 00:49:24 +0900

    1.0.5

--- service_worker.js
+++ service_worker.js
@@ -1,27 +1,18 @@
-// キャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-    '/shinodaiki.github.io/kanji2.github.io//',
-];
+// ファイブラリのインポート
+importScripts('https://shinodaiki.github.io/kanji2.github.io/service_worker.js')
 
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
-});
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+  {
+    url: '/index.html',
+    revision: '12345'
+  },
+  {
+    url: '/style.css',
+    revision: '12345'
+  },
+  { 
+　url: '/manifest.json',
+    revision: '12345'
+  },
+])
