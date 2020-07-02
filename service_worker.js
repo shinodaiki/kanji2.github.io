a38a3f8ce0f6a1383180b65a8eb36941b3fcbbcc
shinodaiki <67748501+shinodaiki@users.noreply.github.com>
2020-07-03 08:34:46 +0900

    1.0.6

--- service_worker.js
+++ service_worker.js
@@ -1,18 +1,30 @@
-// ファイブラリのインポート
-importScripts('https://shinodaiki.github.io/kanji2.github.io/service_worker.js')
+var CACHE_NAME  = "MKit-cache-v1";
+var urlsToCache = [
+    "https://shinodaiki.github.io/kanji2.github.io/style.css",
+    "https://shinodaiki.github.io/kanji2.github.io/index.html",
+    "https://shinodaiki.github.io/kanji2.github.io/service_worker.js",
+    "https://shinodaiki.github.io/kanji2.github.io/manifest.json"
+];
 
-// ファイルのキャッシュ
-workbox.precaching.precacheAndRoute([
-  {
-    url: '/index.html',
-    revision: '12345'
-  },
-  {
-    url: '/style.css',
-    revision: '12345'
-  },
-  { 
-　url: '/manifest.json',
-    revision: '12345'
-  },
-])
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(
+            function(cache){
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
