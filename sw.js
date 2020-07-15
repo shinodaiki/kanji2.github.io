d03d352211594cffc649b21394cf54c2845bc5bb
shinodaiki <67748501+shinodaiki@users.noreply.github.com>
2020-07-15 18:22:51 +0900

    202007151822

--- sw.js
+++ sw.js
@@ -6,3 +6,26 @@ const urlsToCache = [
   './icon.png',
   './style.css',
 ];
+
+// ファイブラリのインポート
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+  {
+    url: '/index.html',
+    revision: '12345'
+  },
+  {
+    url: '/style.css',
+    revision: '12345'
+  },
+  { 
+　url: '/1.js',
+    revision: '12345'
+  },
+  { 
+  url: '/2.js',
+    revision: '12345'
+  },
+])
