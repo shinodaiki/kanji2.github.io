d259a696a004f1c658b52592f24a8260fc4be6af
shinodaiki <67748501+shinodaiki@users.noreply.github.com>
2020-07-15 18:15:50 +0900

    202007151815

--- sw.js
+++ sw.js
@@ -17,38 +17,3 @@ self.addEventListener('install', function (event) {
       })
   );
 });
-
-// リクエストされたファイルが Service Worker にキャッシュされている場合
-// キャッシュからレスポンスを返す
-
-self.addEventListener('fetch', function (event) {
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
-    return;
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Cache Storage にキャッシュされているサービスワーカーのkeyに変更があった場合
-// 新バージョンをインストール後、旧バージョンのキャッシュを削除する
-// (このファイルでは CACHE_NAME をkeyの値とみなし、変更を検知している)
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (!CACHE_NAME.includes(key)) {
-          return caches.delete(key);
-        }
-      })
-    )).then(() => {
-      console.log(CACHE_NAME + "activated");
-    })
-  );
-});
