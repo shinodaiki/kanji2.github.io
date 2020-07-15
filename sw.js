741c4d8f1a0e7f59489189f59f2486591855b04b
shinodaiki <67748501+shinodaiki@users.noreply.github.com>
2020-07-15 21:37:51 +0900

    cache4

--- sw.js
+++ sw.js
@@ -14,22 +14,22 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 workbox.precaching.precacheAndRoute([
   {
     url: '/kanji2.github.io/index.html',
-    revision: '11112'
+    revision: '20000'
   },
   {
     url: '/kanji2.github.io/style.css',
-    revision: '11112'
+    revision: '20000'
   },
   { 
 　url: '/kanji2.github.io/1.js',
-    revision: '11112'
+    revision: '20000'
   },
   { 
   url: '/kanji2.github.io/2.js',
-    revision: '11112'
+    revision: '20000'
   },
   { 
   url: '/kanji2.github.io/icon.png',
-    revision: '11112'
+    revision: '20000'
   },
 ])
