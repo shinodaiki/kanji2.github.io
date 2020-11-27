7c90a71e597fb09fe310cf21875076db9fe5e938
shinodaiki <shinodaiki0531@gmail.com>
2020-11-27 22:09:14 +0900

    202011272209

--- sw.js
+++ sw.js
@@ -5,23 +5,23 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 workbox.precaching.precacheAndRoute([
   {
     url: '/kanji2.github.io/index.html',
-    revision: '20004'
+    revision: '20005'
   },
   {
     url: '/kanji2.github.io/style.css',
-    revision: '20004'
+    revision: '20005'
   },
   { 
 　url: '/kanji2.github.io/1.js',
-    revision: '20004'
+    revision: '20005'
   },
   { 
   url: '/kanji2.github.io/2.js',
-    revision: '20004'
+    revision: '20005'
   },
   { 
   url: '/kanji2.github.io/icon.png',
-    revision: '20004'
+    revision: '20005'
   },
 ])
 
